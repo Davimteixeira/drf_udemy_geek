86374f4b826aa2145f99377dd06362c9c3c72a28
Davi Magalhães <t_dmteixeira@sfiec.org.br>
2024-01-10 09:56:32 -0300

    criando versão 2 de api

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 
-from .views import CursoAPIView, CursosAPIView,AvaliacaoAPIView,  AvaliacoesAPIView
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    CursoAPIView,
+    CursosAPIView,
+    AvaliacaoAPIView,
+    AvaliacoesAPIView,
+    CursoViewSet,
+    AvaliacaoViewSet)
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avalicoes', AvaliacaoViewSet)
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
